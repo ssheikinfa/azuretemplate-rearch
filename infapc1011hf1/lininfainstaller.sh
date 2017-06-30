a6a101503616a45e696e5392a99a450f7c4b0894
vijayanand17 <vijayanand.p@live.com>
2017-06-30 10:42:00 +0530

    1011.HF1

---
+++ infapc1011hf1/lininfainstaller.sh
@@ -0,0 +1,181 @@
+#!/bin/sh
+
+#Script arguments
+domainHost=${1}
+domainName=${2}
+domainUser=${3}
+domainPassword=${4}
+nodeName=${5}
+nodePort=${6}
+
+dbType=${7}
+dbName=${8}
+dbUser=${9}
+dbPassword=${10}
+dbHost=${11}
+dbPort=${12}
+
+sitekeyKeyword=${13}
+
+joinDomain=${14}
+osUserName=${15}
+
+storageName=${16}
+storageKey=${17}
+
+domainLicenseURL=${18}
+
+echo Starting Informatica setup...
+echo Number of parameters $#
+#echo $domainHost $domainName $domainUser $domainPassword $nodeName $nodePort $dbType $dbName $dbUser $dbPassword $dbHost $dbPort $sitekeyKeyword $joinDomain $osUserName $storageName $storageKey $domainLicenseURL
+
+#Usage
+if [ $# -ne 18 ]
+then
+	echo lininfainstaller.sh domainHost domainName domainUser domainPassword nodeName nodePort dbType dbName dbUser dbPassword dbHost dbPort sitekeyKeyword joinDomain  osUserName storageName storageKey domainLicenseURL
+	exit -1
+fi
+
+dbaddress=$dbHost:$dbPort
+hostname=`hostname`
+
+informaticaopt=/opt/Informatica
+infainstallerloc=$informaticaopt/Archive/server
+utilityhome=$informaticaopt/Archive/utilities
+
+
+infainstallionlocown=/home/Informatica
+#mkdir -p $infainstallionlocown/10.1.1
+
+echo Creating symbolic link to Informatica installation
+ln -s $infainstallionlocown /home/$osUserName
+
+infainstallionloc=\\/home\\/Informatica\\/10.1.1
+defaultkeylocation=$infainstallionloc\\/isp\\/config\\/keys
+licensekeylocation=\\/opt\\/Informatica\\/license.key
+
+# Firewall configurations
+echo Adding firewall rules for Informatica domain service ports
+iptables -A IN_public_allow -p tcp -m tcp --dport 6005:6008 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A IN_public_allow -p tcp -m tcp --dport 6014:6114 -m conntrack --ctstate NEW -j ACCEPT
+
+JRE_HOME="$infainstallerloc/source/java/jre"
+export JRE_HOME		
+PATH="$JRE_HOME/bin":"$PATH"
+export PATH
+
+chmod -R 777 $JRE_HOME
+
+cloudsupportenable=1
+if [ "$domainLicenseURL" != "nolicense" -a $joinDomain -eq 0 ]
+then
+	echo Getting Informatica license
+	cd $utilityhome
+	java -jar iadutility.jar downloadHttpUrlFile -url "$(echo $domainLicenseURL | sed -e s/\ /%20/g)" -localpath "$informaticaopt/license.key"
+
+	if [ -f $informaticaopt/license.key ]
+	then
+		cloudsupportenable=0
+	else
+		echo Error downloading license file from URL $domainLicenseURL
+	fi
+fi
+
+
+createDomain=1
+if [ $joinDomain -eq 1 ]
+then
+    createDomain=0
+	# This is buffer time for master node to start
+	sleep 300
+else
+	echo Creating shared directory on Azure storage
+	cd $utilityhome
+    java -jar iadutility.jar createAzureFileShare -storageaccesskey $storageKey -storagename "$storageName"
+fi
+
+echo Mounting the shared directory
+mountdir=/mnt/infaaeshare
+mkdir $mountdir
+mount -t cifs //$storageName.file.core.windows.net/infaaeshare $mountdir -o vers=3.0,username=$storageName,password=$storageKey,dir_mode=0777,file_mode=0777
+echo //$storageName.file.core.windows.net/infaaeshare $mountdir cifs vers=3.0,username=$storageName,password=$storageKey,dir_mode=0777,file_mode=0777 >> /etc/fstab
+
+echo Editing Informatica silent installation file
+sed -i s/^LICENSE_KEY_LOC=.*/LICENSE_KEY_LOC=$licensekeylocation/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^USER_INSTALL_DIR=.*/USER_INSTALL_DIR=$infainstallionloc/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^CREATE_DOMAIN=.*/CREATE_DOMAIN=$createDomain/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^JOIN_DOMAIN=.*/JOIN_DOMAIN=$joinDomain/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^CLOUD_SUPPORT_ENABLE=.*/CLOUD_SUPPORT_ENABLE=$cloudsupportenable/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^ENABLE_USAGE_COLLECTION=.*/ENABLE_USAGE_COLLECTION=1/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^KEY_DEST_LOCATION=.*/KEY_DEST_LOCATION=$defaultkeylocation/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^PASS_PHRASE_PASSWD=.*/PASS_PHRASE_PASSWD=$(echo $sitekeyKeyword | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^SERVES_AS_GATEWAY=.*/SERVES_AS_GATEWAY=1/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^DB_TYPE=.*/DB_TYPE=$dbType/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^DB_UNAME=.*/DB_UNAME=$dbUser/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^DB_PASSWD=.*/DB_PASSWD=$(echo $dbPassword | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^DB_SERVICENAME=.*/DB_SERVICENAME=$dbName/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^DB_ADDRESS=.*/DB_ADDRESS=$dbaddress/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^DOMAIN_NAME=.*/DOMAIN_NAME=$domainName/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^NODE_NAME=.*/NODE_NAME=$nodeName/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^DOMAIN_PORT=.*/DOMAIN_PORT=$nodePort/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^JOIN_NODE_NAME=.*/JOIN_NODE_NAME=$nodeName/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^JOIN_HOST_NAME=.*/JOIN_HOST_NAME=$hostname/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^JOIN_DOMAIN_PORT=.*/JOIN_DOMAIN_PORT=$nodePort/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^DOMAIN_USER=.*/DOMAIN_USER=$domainUser/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^DOMAIN_HOST_NAME=.*/DOMAIN_HOST_NAME=$domainHost/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^DOMAIN_PSSWD=.*/DOMAIN_PSSWD=$(echo $domainPassword | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/ $infainstallerloc/SilentInput.properties
+
+sed -i s/^DOMAIN_CNFRM_PSSWD=.*/DOMAIN_CNFRM_PSSWD=$(echo $domainPassword | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/ $infainstallerloc/SilentInput.properties
+
+# To speed up installation
+mv $infainstallerloc/source $infainstallerloc/source_temp
+mkdir $infainstallerloc/source
+mv $infainstallerloc/unjar_esd.sh $infainstallerloc/unjar_esd.sh_temp
+head -1 $infainstallerloc/unjar_esd.sh_temp > $infainstallerloc/unjar_esd.sh
+echo exit_value_unjar_esd=0 >> $infainstallerloc/unjar_esd.sh
+chmod 777 $infainstallerloc/unjar_esd.sh
+
+echo Installing Informatica domain
+cd $infainstallerloc
+echo Y Y | sh silentinstall.sh 
+
+
+# Revert speed up changes
+mv $infainstallerloc/source_temp/* $infainstallerloc/source
+rm $infainstallerloc/unjar_esd.sh
+mv $infainstallerloc/unjar_esd.sh_temp $infainstallerloc/unjar_esd.sh
+
+if [ -f $informaticaopt/license.key ]
+then
+	rm $informaticaopt/license.key
+fi
+
+echo Changing ownership of directories
+chown -R $osUserName $infainstallionlocown
+chown -R $osUserName $informaticaopt 
+chown -R $osUserName $mountdir
+chown -R $osUserName /home/$osUserName
+
+echo Informatica setup Complete.
---
+++ infapc1011hf1/wininfainstaller.ps1
@@ -0,0 +1,156 @@
+
+Param(
+  [string]$domainHost,
+  [string]$domainName,
+  [string]$domainUser,
+  [string]$domainPassword,
+  [string]$nodeName,
+  [int]$nodePort,
+
+  [string]$dbType,
+  [string]$dbName,
+  [string]$dbUser,
+  [string]$dbPassword,
+  [string]$dbHost,
+  [int]$dbPort,
+
+  [string]$sitekeyKeyword,
+
+  [string]$joinDomain = 0,
+  [string]$masterNodeHost,
+  [string]$osUserName,
+  [string]$infaEdition,
+
+  [string]$storageName,
+  [string]$storageKey,
+  [string]$infaLicense
+)
+
+#echo $domainHost $domainName $domainUser $domainPassword $nodeName $nodePort $dbType $dbName $dbUser $dbPassword $dbHost $dbPort $sitekeyKeyword $joinDomain $masterNodeHost $osUserName $infaEdition $storageName $storageKey $infaLicense
+
+#Adding Windows firewall inbound rule
+echo Adding firewall rules for Informatica domain service ports
+netsh  advfirewall firewall add rule name="Informatica_PowerCenter" dir=in action=allow profile=any localport=6005-6113 protocol=TCP
+
+$shareName = "infaaeshare"
+
+$infaHome = $env:SystemDrive + "\Informatica\10.1.1"
+$installerHome = $env:SystemDrive + "\Informatica\Archive\1011_Server_Installer_winem-64t"
+$utilityHome = $env:SystemDrive + "\Informatica\Archive\utilities"
+
+#Setting Java in path
+$env:JRE_HOME= $installerHome + "\source\java\jre"
+$env:Path=$env:JRE_HOME+"\bin;" + $env:Path
+
+# DB Configurations if required
+$dbAddress = $dbHost + ":" + $dbPort
+
+$userInstallDir = $infaHome
+$defaultKeyLocation = $infaHome + "\isp\config\keys"
+$propertyFile = $installerHome + "\SilentInput.properties"
+
+$infaLicenseFile = ""
+$CLOUD_SUPPORT_ENABLE = "1"
+if($infaLicense -ne "nolicense" -and $joinDomain -eq 0) {
+	$infaLicenseFile = $env:SystemDrive + "\Informatica\license.key"
+	echo Getting Informatica license
+	wget $infaLicense -OutFile $infaLicenseFile
+
+	if (Test-Path $infaLicenseFile) {
+		$CLOUD_SUPPORT_ENABLE = "0"
+	} else {
+		echo Error downloading license file from URL $infaLicense
+	}
+}
+
+$createDomain = 1
+if($joinDomain -eq 1) {
+    $createDomain = 0
+    # This is buffer time for master node to start
+    Start-Sleep -s 300
+} else {
+	echo Creating shared directory on Azure storage
+    cd $utilityHome
+    java -jar iadutility.jar createAzureFileShare -storageaccesskey $storageKey -storagename "$storageName"
+}
+
+$env:USERNAME = $osUserName
+$env:USERDOMAIN = $env:COMPUTERNAME
+
+#Mounting azure shared file drive
+echo Mounting the shared directory
+$cmd = "net use I: \\$storageName.file.core.windows.net\$shareName /u:$storageName $storageKey" 
+$cmd | Set-Content "$env:SystemDrive\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\MountShareDrive.cmd"
+
+runas /user:$osUserName net use I: \\$storageName.file.core.windows.net\$shareName /u:$storageName $storageKey
+
+echo Editing Informatica silent installation file
+(gc $propertyFile | %{$_ -replace '^LICENSE_KEY_LOC=.*$',"LICENSE_KEY_LOC=$infaLicenseFile"  `
+`
+-replace '^CREATE_DOMAIN=.*$',"CREATE_DOMAIN=$createDomain"  `
+`
+-replace '^JOIN_DOMAIN=.*$',"JOIN_DOMAIN=$joinDomain"  `
+`
+-replace '^CLOUD_SUPPORT_ENABLE=.*$',"CLOUD_SUPPORT_ENABLE=$CLOUD_SUPPORT_ENABLE"  `
+`
+-replace '^ENABLE_USAGE_COLLECTION=.*$',"ENABLE_USAGE_COLLECTION=1"  `
+`
+-replace '^USER_INSTALL_DIR=.*$',"USER_INSTALL_DIR=$userInstallDir"  `
+`
+-replace '^KEY_DEST_LOCATION=.*$',"KEY_DEST_LOCATION=$defaultKeyLocation"  `
+`
+-replace '^PASS_PHRASE_PASSWD=.*$',"PASS_PHRASE_PASSWD=$sitekeyKeyword"  `
+`
+-replace '^SERVES_AS_GATEWAY=.*$',"SERVES_AS_GATEWAY=1" `
+`
+-replace '^DB_TYPE=.*$',"DB_TYPE=$dbTYPE" `
+`
+-replace '^DB_UNAME=.*$',"DB_UNAME=$dbUser" `
+`
+-replace '^DB_SERVICENAME=.*$',"DB_SERVICENAME=$dbName" `
+`
+-replace '^DB_ADDRESS=.*$',"DB_ADDRESS=$dbAddress" `
+`
+-replace '^DOMAIN_NAME=.*$',"DOMAIN_NAME=$domainName" `
+`
+-replace '^NODE_NAME=.*$',"NODE_NAME=$nodeName" `
+`
+-replace '^DOMAIN_PORT=.*$',"DOMAIN_PORT=$nodePort" `
+`
+-replace '^JOIN_NODE_NAME=.*$',"JOIN_NODE_NAME=$nodeName" `
+`
+-replace '^JOIN_HOST_NAME=.*$',"JOIN_HOST_NAME=$env:COMPUTERNAME" `
+`
+-replace '^JOIN_DOMAIN_PORT=.*$',"JOIN_DOMAIN_PORT=$nodePort" `
+`
+-replace '^DOMAIN_USER=.*$',"DOMAIN_USER=$domainUser" `
+`
+-replace '^DOMAIN_HOST_NAME=.*$',"DOMAIN_HOST_NAME=$domainHost" `
+`
+-replace '^DOMAIN_PSSWD=.*$',"DOMAIN_PSSWD=$domainPassword" `
+`
+-replace '^DOMAIN_CNFRM_PSSWD=.*$',"DOMAIN_CNFRM_PSSWD=$domainPassword" `
+`
+-replace '^DB_PASSWD=.*$',"DB_PASSWD=$dbPassword" 
+
+}) | sc $propertyFile
+
+
+# To speed up installation
+Rename-Item $installerHome/source $installerHome/source_temp
+mkdir $installerHome/source
+
+echo Installing Informatica domain
+cd $installerHome
+$installCmd = $installerHome + "\silentInstall.bat"
+Start-Process $installCmd -Verb runAs -workingdirectory $installerHome -wait | Out-Null
+
+# Revert speed up changes
+rmdir $installerHome/source
+Rename-Item $installerHome/source_temp $installerHome/source
+
+if($infaLicenseFile -ne "") {
+	rm $infaLicenseFile
+}
+
+echo Informatica setup Complete.
